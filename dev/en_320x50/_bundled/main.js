cdbcd78071f3014be855a0215284468857a11570
Gar <gar@lonelydatum.com>
2021-11-05 09:35:44 -0400

    .

--- dev/en_320x50/_bundled/main.js
+++ dev/en_320x50/_bundled/main.js
@@ -53,14 +53,14 @@ var tl = new TimelineMax({ onComplete: function onComplete() {} });
 
 tl.set(".frame1", { opacity: 1 });
 
-tl.to(".t1", .3, { opacity: 0 }, "+=2");
-tl.from(".t2", .3, { opacity: 0 });
+tl.to(".t1", .4, { opacity: 0 }, "+=2");
+tl.from(".t2", .4, { opacity: 0 });
 
 tl.add("f3", "+=5.3");
-tl.to(".t2", .4, { opacity: 0 }, "f3");
-tl.from(".t4", .5, { opacity: 0 }, "f3");
+tl.to(".t2", .6, { opacity: 0 }, "f3");
+tl.from(".t4", .6, { opacity: 0 }, "f3");
 
-tl.from(".t3", .5, { opacity: 0 }, "+=.4");
+tl.from(".t3", .5, { opacity: 0 }, "+=.3");
 
 },{"../../_common/js/common.js":1}]},{},[2])
 
--- dev/en_320x50/_js/main.js
+++ dev/en_320x50/_js/main.js
@@ -10,12 +10,12 @@ const tl = new TimelineMax( {onComplete:()=>{ }} )
 tl.set(".frame1", {opacity:1})
 
 
-tl.to(".t1", .3, {opacity:0}, "+=2")
-tl.from(".t2", .3, {opacity:0})
+tl.to(".t1", .4, {opacity:0}, "+=2")
+tl.from(".t2", .4, {opacity:0})
 
 
 tl.add("f3", "+=5.3")
-tl.to(".t2", .4, {opacity:0}, "f3")
-tl.from(".t4", .5, {opacity:0}, "f3")
+tl.to(".t2", .6, {opacity:0}, "f3")
+tl.from(".t4", .6, {opacity:0}, "f3")
 
-tl.from(".t3", .5, {opacity:0}, "+=.4")
+tl.from(".t3", .5, {opacity:0}, "+=.3")
