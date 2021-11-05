89e4e05083dcfa8e8ca9311095f135d120895aa5
Gar <gar@lonelydatum.com>
2021-11-05 09:29:55 -0400

    .

--- dev/en_320x50/_bundled/main.js
+++ dev/en_320x50/_bundled/main.js
@@ -54,12 +54,13 @@ var tl = new TimelineMax({ onComplete: function onComplete() {} });
 tl.set(".frame1", { opacity: 1 });
 
 tl.to(".t1", .3, { opacity: 0 }, "+=2");
-tl.from(".t2", .5, { opacity: 0 }, "+=.1");
+tl.from(".t2", .3, { opacity: 0 });
 
-tl.to(".t2", .3, { opacity: 0 }, "+=5.3");
-tl.from(".t4", .3, { opacity: 0 }, "+=.1");
+tl.add("f3", "+=5.3");
+tl.to(".t2", .4, { opacity: 0 }, "f3");
+tl.from(".t4", .5, { opacity: 0 }, "f3");
 
-tl.from(".t3", .5, { opacity: 0 }, "+=.1");
+tl.from(".t3", .5, { opacity: 0 }, "+=.4");
 
 },{"../../_common/js/common.js":1}]},{},[2])
 
--- dev/en_320x50/_js/main.js
+++ dev/en_320x50/_js/main.js
@@ -4,18 +4,18 @@
 import {init} from '../../_common/js/common.js'
 
 
-const tl = new TimelineMax({onComplete:()=>{
-	
-}})
+const tl = new TimelineMax( {onComplete:()=>{ }} )
 
 
 tl.set(".frame1", {opacity:1})
 
 
 tl.to(".t1", .3, {opacity:0}, "+=2")
-tl.from(".t2", .5, {opacity:0}, "+=.1")
+tl.from(".t2", .3, {opacity:0})
 
-tl.to(".t2", .3, {opacity:0}, "+=5.3")
-tl.from(".t4", .3, {opacity:0}, "+=.1")
 
-tl.from(".t3", .5, {opacity:0}, "+=.1")
+tl.add("f3", "+=5.3")
+tl.to(".t2", .4, {opacity:0}, "f3")
+tl.from(".t4", .5, {opacity:0}, "f3")
+
+tl.from(".t3", .5, {opacity:0}, "+=.4")
