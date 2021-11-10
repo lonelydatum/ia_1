bece9c1554ae1725fd4d4f3de9a501784553a40f
Gar <gar@lonelydatum.com>
2021-11-10 18:30:42 -0500

    ON

---
+++ dev/fr_320x50/_js/main.js
@@ -0,0 +1,21 @@
+
+
+
+import {init} from '../../_common/js/common.js'
+
+
+const tl = new TimelineMax( {onComplete:()=>{ }} )
+
+
+tl.set(".frame1", {opacity:1})
+
+
+tl.to(".t1", .4, {opacity:0}, "+=2")
+tl.from(".t2", .4, {opacity:0})
+
+
+tl.add("f3", "+=5.3")
+tl.to(".t2", .6, {opacity:0}, "f3")
+tl.from(".t4", .6, {opacity:0}, "f3")
+
+tl.from(".t3", .5, {opacity:0}, "+=.2")
