4b2fae193fb463a2c6cd2af2dd1e3dc300eafb05
Gar <gar@lonelydatum.com>
2021-11-22 11:21:39 -0500

    ON

--- peter_laurie_en/peter_en_1080x1080/_js/main.js
+++
@@ -1,6 +0,0 @@
-import {init2, size} from '../../_common/js/common.js'
-
-init2()
-
-module.exports = {};
-
--- peter_laurie_en/peter_en_300x250/_js/main.js
+++
@@ -1,6 +0,0 @@
-
-
-
-import {init2} from '../../_common/js/common.js'
-
-const tl = init2()
